ca265997314e80317100ac7422216e674814a30f
Dima <diploma.admn.hotel@gmail.com>
2025-04-10 12:10:02 +0300

    commit 1

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('d' * 3);
+
+alert('DH852');
+
+const testConfirm = confirm('Are ?');
+
+console.log(testConfirm);
+
+const testPrompt = prompt('Ar ar?', 'ar ar');
+
+console.log(testPrompt);
+
+setTimeout(() => {
+  document.write(testPrompt);
+}, 3000);
